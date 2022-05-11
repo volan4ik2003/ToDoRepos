29c84462f5bb97714e855ec824847f1f23567bcd
volan4ik2003 <yanc359@gmail.com>
2022-05-11 20:56:47 +0300

    Added new function NewList

--- ConsoleApplication6/ConsoleApplication6.cpp
+++ ConsoleApplication6/ConsoleApplication6.cpp
@@ -3,8 +3,6 @@
 
 using namespace std;
 
-
-
 struct TodoItem
 {
     int id = 0;
@@ -17,11 +15,52 @@ struct TodoList
 {
     int id = 0;
     string name = " ";
-    TodoItem* todoItems = new TodoItem[];
+    TodoItem* todoItems;
+
+    TodoList(int todoItems_size)
+    {
+        todoItems = new TodoItem[todoItems_size];
+    }
 };
+
+
+TodoList NewList()
+{
+    int todoItems_size;
+
+    cout << "Enter TodoList size: ";
+    cin >> todoItems_size;
+
+    TodoList list(todoItems_size);
+    
+    cout << "Enter list id: ";
+    cin >> list.id;
+
+    cin.ignore();
+    cout << "Enter list name: ";
+    getline(cin, list.name);
+
+    for (int i = 0; i < todoItems_size; i++)
+    {
+        cout << "Enter Item id: ";
+        cin >> list.todoItems[i].id;
+
+        cin.ignore();
+        cout << "Enter Item name: ";
+        getline(cin, list.todoItems[i].name);
+
+        cout << "Enter Item description: ";
+        getline(cin, list.todoItems[i].description);
+    
+        cout << "Enter Item due_date: ";
+        getline(cin, list.todoItems[i].due_date);
+    }
+    return list;
+}
 
 int main()
 {
-    TodoList list;
+    NewList();
+    return 0;
 }
 
