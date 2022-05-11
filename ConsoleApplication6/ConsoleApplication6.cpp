453451d02e768af3fbf3b516c1c4c3f78207e56c
volan4ik2003 <yanc359@gmail.com>
2022-05-11 20:04:09 +0300

    Added to structures

--- ConsoleApplication6/ConsoleApplication6.cpp
+++ ConsoleApplication6/ConsoleApplication6.cpp
@@ -3,8 +3,23 @@
 
 using namespace std;
 
+struct TodoItem
+{
+    int id;
+    string name;
+    string description;
+    string due_date;
+};
+
+struct TodoList
+{
+    int id;
+    string name;
+    TodoItem todoItems[];
+};
+
 int main()
 {
-    std::cout << "Hello World!\n";
+   
 }
 
